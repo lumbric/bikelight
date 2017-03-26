4d826d76d65ec294e2ae3b271eb5a5ca296ba99d
lumbric <lumbric@gmail.com>
2017-03-27 00:47:20 +0200

    Add class for Led
    Refactoring for more overview. This implements something like poor man threads.

--- Blink.ino
+++ Blink.ino
@@ -7,8 +7,8 @@
 
 
 // assymetric blink intervall for breathing mode
-#define BREATH_IN_US 4000
-#define BREATH_OUT_US 2000
+#define BREATH_IN_MS 4800
+#define BREATH_OUT_MS 2000
 
 // assymetric blink intervall for flashing mode
 #define FLASH_ON_US 1000
@@ -24,10 +24,10 @@
 #define BRAKE_LIGHT_DURATION_MS 3000
 
 // dimm light for all modes when no brake, int between 0-255
-#define MAX_LIGHT_NO_BRAKE 180
+#define MAX_LIGHT_NO_BRAKE 255
 
 // minimum light when flashing/breathing, int between 0-255
-#define MIN_LIGHT_NO_BRAKE 90
+#define MIN_LIGHT_NO_BRAKE 30
 
 // PINs
 #define LED_PIN 0
@@ -53,85 +53,105 @@ byte light_mode = 0;
 
 
 bool brake() {
-  bool brake_on = false;
-  while (digitalRead(BRAKE_PIN) == LOW) {
-    analogWrite(LED_PIN, 255);
-    delay(BRAKE_LIGHT_DURATION_MS);
-    brake_on = true;
-  }
-  return brake_on;
+    bool brake_on = false;
+    while (digitalRead(BRAKE_PIN) == LOW) {
+        analogWrite(LED_PIN, 255);
+        delay(BRAKE_LIGHT_DURATION_MS);
+        brake_on = true;
+    }
+    return brake_on;
 }
 
 
 void run_light() {
-  switch (light_mode) {
+    switch (light_mode) {
     case 0:
-      break;   
-  }
+        break;
+    }
 }
 
 
 void switch_mode(int mode) {
-  light_mode = mode;
+    light_mode = mode;
 }
 
 
 void go_sleep() {
-  // TODO fade out led here
-  analogWrite(LED_PIN, 0);
-  delay(2000);
-  analogWrite(LED_PIN, 255);
-  delay(100);
-  analogWrite(LED_PIN, 0);
+    // TODO fade out led here
+    analogWrite(LED_PIN, 0);
+    delay(2000);
+    analogWrite(LED_PIN, 255);
+    delay(100);
+    analogWrite(LED_PIN, 0);
 
-  GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
-  PCMSK |= _BV(PCINT0);                   // Use PB3 as interrupt pin
-  ADCSRA &= ~_BV(ADEN);                   // ADC off
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+    GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
+    PCMSK |= _BV(PCINT0);                   // Use PB3 as interrupt pin
+    ADCSRA &= ~_BV(ADEN);                   // ADC off
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
 
-  sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
-  sei();                                  // Enable interrupts
-  sleep_cpu();                            // sleep
+    sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+    sei();                                  // Enable interrupts
+    sleep_cpu();                            // sleep
 
-  cli();                                  // Disable interrupts
-  PCMSK &= ~_BV(PCINT0);                  // Turn off PB3 as interrupt pin
-  sleep_disable();                        // Clear SE bit
-  ADCSRA |= _BV(ADEN);                    // ADC on
+    cli();                                  // Disable interrupts
+    PCMSK &= ~_BV(PCINT0);                  // Turn off PB3 as interrupt pin
+    sleep_disable();                        // Clear SE bit
+    ADCSRA |= _BV(ADEN);                    // ADC on
 
-  sei();                                  // Enable interrupts
+    sei();                                  // Enable interrupts
 } 
+
 
 ISR(PCINT0_vect) {
     // This is called when the interrupt occurs, but I don't need to do anything in it
 }
 
 
+class Led {
+    public:
+        Led() {};
+        void update() {
+            unsigned long t = millis();
+            
+            if (t > start_ms + BREATH_IN_MS + BREATH_OUT_MS) {
+                // period over, new start
+                start_ms = t;
+            }
+
+            if (t < start_ms + BREATH_IN_MS) { 
+                brightness = (double) (MAX_LIGHT_NO_BRAKE - MIN_LIGHT_NO_BRAKE)
+                    * (t - start_ms) / BREATH_IN_MS + MIN_LIGHT_NO_BRAKE; 
+            } else {
+                // FIXME do not use double, use int or so - can this overflow?
+                brightness = (double) (MIN_LIGHT_NO_BRAKE - MAX_LIGHT_NO_BRAKE)
+                    * (t - BREATH_IN_MS - start_ms) / BREATH_OUT_MS 
+                    + MAX_LIGHT_NO_BRAKE; 
+            }
+
+            analogWrite(LED_PIN, brightness);
+        };
+    private:
+        byte brightness;
+        unsigned long start_ms;  // start of period
+};
+
+
+Led led;
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(BRAKE_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(BRAKE_PIN, INPUT_PULLUP);
 
     // attachInterrupt(0, pin2_isr, LOW);
 }
 
 
 void loop() {
-  byte light_val;
-  // != instead of <= becaus of overflow
-  for (light_val = MIN_LIGHT_NO_BRAKE; light_val != MAX_LIGHT_NO_BRAKE; light_val++) {
-    analogWrite(LED_PIN, light_val);
-    delayMicroseconds(BREATH_IN_US);
-    //brake();
+    // bool just_woke_up = millis() < 1000;
+    led.update();
+    brake();
+
     if (digitalRead(BUTTON_PIN) == LOW)
-      go_sleep();
-  }
-  for (light_val = MAX_LIGHT_NO_BRAKE; light_val != MIN_LIGHT_NO_BRAKE; light_val--) {
-    analogWrite(LED_PIN, light_val);
-    delayMicroseconds(BREATH_OUT_US);
-    //brake();
-    if (digitalRead(BUTTON_PIN) == LOW)
-      go_sleep();
-  }
-  
+        go_sleep();
 }
